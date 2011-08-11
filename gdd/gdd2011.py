e0c57f89b428e538a612baec88b3bd37e0d03971
Rodrigo Nossal <nossal@gmail.com>
2011-08-11 10:14:09 -0300

    Provinha GDDBR 2011

---
+++ gdd/gdd2011.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+txt_a = """lrcvxf mzklwql cldcs qjtqfdmf zzrcb bjn qqkzwr djnd vqzvzq tclqlcnj sfp tkwncb wtmp lkmgsflz xqtx dzd bwmz cmlrnbk dckxjgfc gvhh sxnsgm hwlpkmv wmflwn tlqfp fvgq hhztlv tqxhgvp xzfmm svq rwgxlw pxcnl jkqmw dbzlkvws dxqncxz xgvfxphb bkgpv tvfvn dslvfcj pkpzx pvtjvp tmfqcdjk jxx mvn phl lmppz znbx tzt khmch qhqq rjswkfw pjdb ddqlwfkt bvsh wbbwj cqqjnkh dfqddsp kbnw qmzljr kskr rhl psfvkzn gqppnw gnqshrwt pjc wkvgtq krlbcpcj qcfknt fmj kflwgd wpf thdkns qsbwkkz ptbwk nvksmdms bvmllzdw hbwwfs kklpn ntp zhr fxwp jqn cphvk gkldb mgfdgv ghkqtptt dklx ljhlxcj mkjrf vknlg vgst bwhvhsbv tlqtvdvk mgnclz cwms nglkk hvvzm vbvd dvmp zxwszs jpbggpz fbj sbrcqwg rxxn fjhxxq nmj czvkbfp nskqlrpd phbpwnms jpg jtdb vjxpq ngkhxsc vjnptj slrkhpl ftztbb wswhnzkg jxsvzj wlfm lmlllkpc hqwq mmmw fcz lzvl hjr ghtwgrr krwq qxxczgxp tpxqrdxn sgf prl nwtbbspg gpbsj kchjjsp cgvjfm mcxxgln frjtl xtzjst swtmnvgv prlj fjvxjsm fbrlg qnsschb tsp rbd bprzj nzdmmckq jvtfknz xmgd bqlgc jcpgs lqpjpn jtpkkc kbwfm vqnjbl cxs tncdjh vpmgps hbwvbhwq llgdv msv hrzqm gtxkvgp rlpv bphmm pzbvj zjxchz gll krcgvz wjdbsg gnmmc hnlskg shsqvzh cdc zhnhlfh xcwjj fgp hgnt tlkcfh rtgklgj bpz rntqsjxt bgb kqdrqn cvqjrs jcts tvtvj qwqmrck lhbfj dtknh vknllzpb fgzdf qvd fxrmxvj vsjggdb rxxb bcrqd zgdvlnvq xvb qhrfg xkcpwj gmcclw rlvmtgjn vczhbzt dmhhqm cpgnz zmmmgwd bntndx qbqkv bnpqqhd mjvc rzwknct klh wvgghmw zjgss jwd gcscxcqz mjhflvpc spmskfx wbdt jdf zrj lqztb rklkstmf mddcb vrt wfckph fwhhn xfmrgdl cbck fbgsll xhn mtsqvz lzjlqf xgc kqrkftkk jflsdbfh pdcrzzh dsdsdhc vbnxpjv dlc fcxcdvjt dlfqj zgrvcswc lzc lrpl lwcqqxb gcdv xksxs ljgzcvh zkgv qhvj ljxsk nwpxzt drsslt cfrqj dnqdskn hmbjlb xmbml ddbsh jktr fjpm vnlmtvh ckmvzjk pvxplxnt wllvqps lvqw ztb jdmv lfbrcmhn gqfwpvk rftkjbh mnjfv kphcm bgr vmc qhdmbtm htfkvsct mqvhfgb pwbqrc bpsscfs zkcsn sdfj jst dcx gcs vhqv swrcvsds kttx tbptv zljrmpnm bmxxlkwt tzq ddcw zfshgbmx ctlkwrm tml rfssgcv csrsjnn njwrkhj kxfwhht jbcdbdpv bqbls hxqhqbjn bbnxw plcx lxfk kpnhldk hvqm xmqq ngqwvvzl wvrfjv hqbrvhnx zcgzx qnpvstv zknkh hwh bxc kvnzwbrw ptrkdb cssq mktzfjqs nwtvbw qxrjrvhz fmkpbk dqpsfkfh jsq wgtk dwtlnrdr lcd rjsnhx twdmxfnr fscpbqgb jxvkqv hbpsvwgf qqddcr prdqglm zgwvzvnj vhldjzp vlmf rrjwslsw hflh bmfdqcxv ntfnw ctvvwt msfjvfjl lwmpvsrz psvs btcjdk htxn khdvwc hxvdhnls rswpvpq sxnck bpnphd dvzqskcw hnhvzstv cgtv wvvjswm swwv ttfpl fdhqk mwpj dbkhxlr fxxwcw vvjfs zpcpz hjdq fdwxt bskljvhq cbtsdthv hlrnh mdm nsk xcngx brs pkhj mhhzr nln wpsskw khprpcvt xfddvs ljmcdt dhq vvdlvn kgxqrk ttwxxzb cgrhx hjnwgwvw bqfwfd gptjk wnlpd cpnv gnxgqp ttchfwg spfvbjld lbpl jkblrxgj ggqppr rzg flrkk jfhztgg klnhk fzdfndgv vld wvrsnv wdrpnd jplz czxkbhnr fnt dbnj fpv drvwskgc swlkgv gcd zvcqhgr rhrbzg msrtvgbn cjwvlg bfqkctwv gkdz gxhl tvfkmfb mbfmfdjl wlzxxwqd tgzf dkf kqflkrtl bvn xwdm pjqmjf qdmkw kjhth dxb tkzx hlssb qbx nvgslt xdrg ccdlwm mnnht jzmmlxx wkpmhllh jqflbb fwcqf dhgxvtnh qsqjthv wgvksmh ftkdddh zmfbd jqh khcds jgwcgx sxhms xwmmqdf vxnjn cndfvdg mcfsmbqf vjtwgrwc tdfvzsfx bclpkmq bmtphtg fxm ksvvqgtf bxwfgdgl pch sczgrzgq kbhtbsd sdrjclz lxhv vsjld csjsg xll whmcvrv sjzftkff txlskqhz mgh sbztsqg sjlcrr cbj nlxj wnj dpqfwdxd qgcz hhdnjv mrzc thts fxc rvz szg srp pthldql pzgjd mjs btkt tcwpj hlrhcx nptbbhtp lwn rjl fqz cms kzwxvl zzb qxhqndhs lsj xwxxfnmr zkwchqh pnmtzqb vswzvjj hgwmv vpxsvch fwfcpl nrpl bgcsrfq zxzphbfd hqxrz gsgdc xmqtm mjfhs tkt gjfk rhcnpq xbppjdkx nbt gkmrwpv txjwfk czbdhl kvvvlr mzsd dsfbkxlb gfh crc nlgvtr knvjpmg nvkzfwts wdngswz ghvrxjdc jztcrtg bvgwcs gtpghfx qftd bltd xgq kdnrsq ztljsqq bfxk dpn lkphh mgftwz vxcjkwp fcllnp zfrr cpkfrg jhzknx brnnw tthjbl lrgsbcqx wdxndc ctxsmkpz tdvgxhv dghk hbtv fgf qsc kjt dwzd qnzb tdzcrhh xpzbktm qsgqmxgb vdzwkqg hjd szdn rphzs cmwz stl cwqz dqd mfcxsh rpw vfc ltpt lvmj xvhxntq tgkp rhpwg zdcvtr rpwvnpq xpmpb knvx hkngvk djsf wfwk sfdnl jvbz nfmt prlb dvvfbmpz sgcqs pxvjmts"""
+
+voc_txt_a = """xdrg xbppjdkx xwxxfnmr xwdm xwmmqdf xfddvs xfmrgdl xhn xmbml xmgd xmqq xmqtm xvb xvhxntq xzfmm xgvfxphb xgc xgq xll xpmpb xpzbktm xcngx xcwjj xkcpwj xksxs xqtx xtzjst dxb dxqncxz ddbsh ddcw ddqlwfkt djnd djsf dbnj dbzlkvws dbkhxlr dnqdskn dwzd dwtlnrdr drvwskgc drsslt dfqddsp dhgxvtnh dhq dmhhqm dvmp dvvfbmpz dvzqskcw dzd dghk dlfqj dlc dpn dpqfwdxd dcx dckxjgfc dkf dklx dqd dqpsfkfh dsdsdhc dsfbkxlb dslvfcj dtknh jxx jxvkqv jxsvzj jdf jdmv jbcdbdpv jwd jfhztgg jflsdbfh jhzknx jvbz jvtfknz jzmmlxx jztcrtg jgwcgx jpbggpz jpg jplz jcpgs jcts jkblrxgj jkqmw jktr jqn jqflbb jqh jsq jst jtdb jtpkkc bxwfgdgl bxc bjn bbnxw bnpqqhd bntndx bwhvhsbv bwmz brnnw brs bfxk bfqkctwv bmxxlkwt bmfdqcxv bmtphtg bvn bvmllzdw bvgwcs bvsh bgb bgr bgcsrfq bltd bpnphd bprzj bphmm bpz bpsscfs bcrqd bclpkmq bkgpv bqbls bqfwfd bqlgc bskljvhq btcjdk btkt njwrkhj nbt nwpxzt nwtbbspg nwtvbw nrpl nfmt nmj nvgslt nvkzfwts nvksmdms nzdmmckq nglkk ngkhxsc ngqwvvzl nlxj nln nlgvtr nptbbhtp nsk nskqlrpd ntfnw ntp wdxndc wdngswz wdrpnd wjdbsg wbdt wbbwj wnj wnlpd wfwk wfckph whmcvrv wmflwn wvrfjv wvrsnv wvvjswm wvgghmw wgvksmh wgtk wlfm wlzxxwqd wllvqps wpf wpsskw wkvgtq wkpmhllh wswhnzkg wtmp rxxb rxxn rjl rjsnhx rjswkfw rbd rntqsjxt rwgxlw rrjwslsw rfssgcv rftkjbh rhrbzg rhl rhpwg rhcnpq rvz rzwknct rzg rlvmtgjn rlpv rpw rpwvnpq rphzs rklkstmf rswpvpq rtgklgj fxxwcw fxwp fxrmxvj fxm fxc fdwxt fdhqk fjhxxq fjvxjsm fjpm fbj fbrlg fbgsll fnt fwfcpl fwhhn fwcqf frjtl fmj fmkpbk fvgq fzdfndgv fgf fgzdf fgp flrkk fpv fcxcdvjt fcz fcllnp fqz fscpbqgb ftztbb ftkdddh hxvdhnls hxqhqbjn hjd hjdq hjnwgwvw hjr hbwwfs hbwvbhwq hbpsvwgf hbtv hnhvzstv hnlskg hwh hwlpkmv hrzqm hflh hhdnjv hhztlv hmbjlb hvvzm hvqm hgnt hgwmv hlrnh hlrhcx hlssb hkngvk hqxrz hqbrvhnx hqwq htxn htfkvsct mddcb mdm mjfhs mjhflvpc mjvc mjs mbfmfdjl mnjfv mnnht mwpj mrzc mfcxsh mhhzr mmmw mvn mzklwql mzsd mgnclz mgfdgv mgftwz mgh mcxxgln mcfsmbqf mkjrf mktzfjqs mqvhfgb msrtvgbn msfjvfjl msv mtsqvz vxnjn vxcjkwp vdzwkqg vjxpq vjnptj vjtwgrwc vbnxpjv vbvd vnlmtvh vrt vfc vhldjzp vhqv vmc vvdlvn vvjfs vgst vld vlmf vpxsvch vpmgps vczhbzt vknlg vknllzpb vqnjbl vqzvzq vsjggdb vsjld vswzvjj zxwszs zxzphbfd zdcvtr zjxchz zjgss znbx zrj zfrr zfshgbmx zhnhlfh zhr zmfbd zmmmgwd zvcqhgr zzb zzrcb zgdvlnvq zgwvzvnj zgrvcswc zljrmpnm zpcpz zcgzx zknkh zkwchqh zkgv zkcsn ztb ztljsqq gxhl gjfk gnxgqp gnmmc gnqshrwt gfh ghvrxjdc ghkqtptt ghtwgrr gmcclw gvhh ggqppr gll gpbsj gptjk gcd gcdv gcs gcscxcqz gkdz gkmrwpv gkldb gqfwpvk gqppnw gsgdc gtxkvgp gtpghfx lxfk lxhv ljxsk ljhlxcj ljmcdt ljgzcvh lbpl lwn lwmpvsrz lwcqqxb lrgsbcqx lrpl lrcvxf lfbrcmhn lhbfj lmlllkpc lmppz lvmj lvqw lzjlqf lzvl lzc llgdv lcd lkmgsflz lkphh lqztb lqpjpn lsj ltpt pxvjmts pxcnl pdcrzzh pjdb pjc pjqmjf pnmtzqb pwbqrc prdqglm prl prlj prlb phbpwnms phl pvxplxnt pvtjvp pzbvj pzgjd plcx pch pkhj pkpzx psfvkzn psvs ptbwk ptrkdb pthldql cxs cdc cjwvlg cbj cbck cbtsdthv cndfvdg cwms cwqz crc cfrqj cmwz cmlrnbk cms cvqjrs czxkbhnr czbdhl czvkbfp cgrhx cgvjfm cgtv cldcs cpnv cphvk cpgnz cpkfrg ccdlwm ckmvzjk cqqjnkh csjsg csrsjnn cssq ctxsmkpz ctvvwt ctlkwrm kxfwhht kdnrsq kjhth kjt kbnw kbwfm kbhtbsd knvx knvjpmg krwq krlbcpcj krcgvz kflwgd khdvwc khmch khprpcvt khcds kvnzwbrw kvvvlr kzwxvl kgxqrk klnhk klh kpnhldk kphcm kchjjsp kklpn kqdrqn kqrkftkk kqflkrtl ksvvqgtf kskr kttx qxxczgxp qxrjrvhz qxhqndhs qdmkw qjtqfdmf qbx qbqkv qnzb qnpvstv qnsschb qwqmrck qftd qhdmbtm qhrfg qhvj qhqq qmzljr qvd qgcz qcfknt qqddcr qqkzwr qsbwkkz qsgqmxgb qsc qsqjthv sxnck sxnsgm sxhms sdrjclz sdfj sjzftkff sjlcrr sbrcqwg sbztsqg swwv swrcvsds swlkgv swtmnvgv srp sfdnl sfp shsqvzh svq szdn szg sgf sgcqs slrkhpl spfvbjld spmskfx sczgrzgq stl txjwfk txlskqhz tdfvzsfx tdvgxhv tdzcrhh tbptv tncdjh twdmxfnr thdkns thts tmfqcdjk tml tvfvn tvfkmfb tvtvj tzq tzt tgzf tgkp tlkcfh tlqfp tlqtvdvk tpxqrdxn tcwpj tclqlcnj tkwncb tkzx tkt tqxhgvp tsp ttwxxzb ttfpl tthjbl ttchfwg"""
+
+txt_b = """hqqfq vnwttl nwqq lzsj ppst fwtqb jfjktp rcdmhtg lqz glrslppr tbkks krcn tbphswb lhf qdgq zsx xdpw pqj cdhms ktb lzd rknxmc lmvkjgw qdnkmvjc kwtltss qwwgr rtxcx vcplp trcgsp mrdlvsr xvct rwnpj ccst lqpr pfhlfx tgfgffm tkhrg kqggnvcx qsh phbtrfmw tkxnd gmn rxgnbb cqlnm dmxqk tvdq nnk rbppzbq jtsqtfgv rmkwmmns txldnkf lnswq kxwcbr kkv qvmrgfd kqnblnr zgxv crggdfwp kjmhfz lxvb rrlfqt pdrk fcfjzl nzxfjvjg pzjb xcwpjjk pdc nrjzmdxq jxpqgfgd hfzdmlpn xfgw mzg bnrhhq pxtmjzpg mswv wnp gzj jtbztkk qmlbck jbbhnrxq qvrvkqdw wmb jqddpb pnkm mszrh mlwlvhf knq rzh sfgws dxxtf dnbgxdvn jfhclwxm nglgvg fbfszsbl nlbpd nqm jkl dsmvgdb xtst ljzvdtg kjfw skdxjnrq mxvvql dnnvz crgmqrbk zgmhbg zhjtch dxl qrblpg qxgkgp dgq cknj kfqwdwft bftd xtq qbw vrf szmrjvm smdjt hwn kjrdnjk hrbntfng dqgcpcz jcsgw pmjt wdcs xnqkwlpl vsbhxxl vwd wlpk xcq kkphpwnc qdqtsl whmkjs dzxwl lsxcx ghhvpwq bbclqqn sltrqj bpwwzfl bhllpzbf rkdtks tbdhb frlfzdq dszv rrvmvz tjkg lfl cqckb bdmbwb wwbx kkq cpl gnfg gzvvx pbqznc bxxtjr wmz bhrbwtmd kclrdpbg hwb ljhdjrxf mnvsmgzt mvlhzct gxkcc bgjcrg rxzsf gqrrfq cwsr pkcfbs zdf cwvcs rpnwzdt ltdsftmb jmbffmw xrm qvjvz hzpz szvfb sgxhnrzz ctxkl nbpxpxrj nsjdhrd tsjlljnf xzqcq qrhgbkqr rsgk vgwprz szbrk rrzc lzfstmx mfcvpnn wbfqlsmq xgrjhc chjpzv spdkc sxbv zjbc vlq spwbd wgnxmrw sldq hchvq pskhnrx bzztrvmg mhg gzsfkmx mnqssdsz hqzncrrj qhw kqzf bdwcbcft rqhwp ltz pfbmnrr vwwpsn xtzzvs xshlf tvzf rfcdn tvcksg rdkgmq lpgvc mtpwns fgvvcbj clqkdn gkfhqz zqlgpp bwkptv nrszvvn ctstbr zznggk thh fnqwbzlv qkkmnnx bfqgk qdq ssvxhzx lnskbhd ppmb pdmtrxk tkklqgl llqphm mbj zvfmmmwk swwh spdnwfv bhqdz mgjgtkl xdj kwrrq wcqqmn fhdtpfb cwg mzgnbrx cccbqq dcdbnlc fshgmpxf ztdxrlzh pglls kqgv sblct qnsljp jgxww gsh xbs rhmtnzzk qpmmv xkngz fbt gqqp zhb lgh hqs rztjqmcg rrnfbg hlzv vdlsmn pmgs bdhlslfh mkwrhnq zslvr wvssg lhstlf hdczcw qztkgfw mlfwl bclxnxm ssc hfgn nxwwwx qnpxdwj cvhzwmnc mvwhgw cgjwwvr crc frrzqb jlflxzjw svqffw kklrlxzl qxwqfnr hqhklpz mrgw dbrrmjv xxw qwdmxfj tjmxtxvd gmvd nhbxlzlv zsbvcpvm kjfctfl cbljdvjk fnmskg qjv qtp njbm wwnlf xbjjcm wcvgb znp rpgvjh bhzwtwwd pgs ggfb skrv pshsrx hckxp bhhv pstds xsvf fknmkzk hxb vgh fxdpcpmx zbxjrd dxt cmmzcm rwb stgkqrq sjtlprqj kkqptc vkvsjl frnlz stt dtvrxbrd phrvrzcq zbbql mxt vsdnl nbvdfr gnskzjb cvcf qdzh jncv dhwjhjc pbbn slwnbvc jjdn xtmg vqz gdnr pnvkn zvkgmmzw qwtpjf ldq qzbnq zwgxxcm pmsr lbm xhjlgwwn rxrskqhv pnxhbs rdv gplprl wgp dhvrfzhs gbpxxvr nkgzlnf fxdxgjt llrcfvgd wjz rdvbc wrkfxljz qbpccz nxb bplhf rhd qzhm znfjjcww thmxwrt hptd fpzph grshvw tnjbctx vkldc rntljkd fkjsfx vfpjhr npcvjp kvqwqlr pfmtw czkn xbd wgkvkt ltzxvk pzmwm sgcgh mnvmgz pbvg bnm bcmcpjr nzxgvl mtsdsmr nckb zcdffpzn zbnd dqwqgv rmv tvtcxkm dttd xkxzx zxwknmpv mrfzlc xrbk dqwvzmc bjs mjkgk hjggmxwm lwtd qnvlxs svbfmmv pmhcpsrl rcshdlqz bqvbg hrnntbjl jxqpgfbq qpcvl djgb bljxrbg qff vchnrh ftqhvpf zwnvrm pwrz swpnrvp fjlhvk ftgr xxwnvz spqfzq pxd wfmdhp njprts vmjk wvt sfnxh mgxrl xflpp njfdpjk bdt pswlbd shkgn vcrxbtdh wnx vjfjzfkj dvmtcqhb jgjgrws gxb svmds dknjlg zwzb zjfw brd vbdhfzvg mwvfmsg rxlzmkp lhjwrb hhgd rgdtcrq lhcjf fknvz lddkvkfq qvzxkgxt gmncz nmkhgzmx qbkh chmhbbvv xhflcglb zclqcfc ddlll sqp jnfvj rhm vlhjqrm kkrdqbmh wxhfqz lzlc zlqwrjxq lzbl zlnjkgw vfcxpkp qzd pbbkbxt bnnhjtn jwzffpmh jxzg kdqkp kgrbtwwg pckcrbgt rpw hwldt lkf zfxnzr kwqvrlhr vfqn tlzjmdzs hcbzz ccbc tjbq pfqhlvrb bqn ksgqxxqc kkv wtmqx mcj lbmtt hbhwbgg gxlkztr xxfjl qxjpkfk kvbkcdg hgt nkrjrx wvk tfpjtwv bphv mpnkv dwgxt sfzr rvpt zlgczw mnmmmjvc mpjpp nnzr bshtl zvgdd sfbnn gvkm qfz hnkdw jhsblkrq nztk stctxrnt vljzrv zxwbf kmxm xtnvxskd jzlcpwlf gqkzgwxg wbkrxxzn qcl ztsct spwz vht vkhf wwwp lvhc wkqthvsl zkc dtxxmdj chm zqzwz jhtn swzjxtc bvcsq gnhcvgh rzkv rsznkfp dwzd tpzz pxkgtplk qzlhvc ngdjqh kbl rtht mvmpck fwrmdqff lxgthw qrzsxw gjhcxrds qrctr pqxjxzm chw thvb zlpvdls btqqp zqzq zmdmzph mwthvqm hvq xqv"""
+
+foo = ['l','h','v','r','n']
+
+def preposicoes():
+    CARACTERS = 5
+    preps = []
+    
+    words = txt_b.split()
+    for word in words:
+        if (word.__len__() == 5 and word.count('c') == 0 and word[-1] in foo):
+#            print word
+            preps.append(word)
+    
+    #print preps
+    print 'N Preps: ', preps.__len__()
+
+def verbos():
+    CARACTERS = 8
+    verbos = []
+    verbos1 = []
+    
+    words = txt_b.split()
+    for word in words:
+        if ( (word.__len__() >= 8) and (word[-1] not in foo) ):
+            if (word[0] in foo):
+                verbos1.append(word)
+#            print word
+            verbos.append(word)
+            
+    #print verbos
+    print 'N verbos: ', verbos.__len__()
+    print 'N verbos 1: ', verbos1.__len__()
+    
+order = 'x d j b n w r f h m v z g l p c k q s t'.split()
+def cmp_order(x, y):
+    ret = 0
+#    word = []
+    xl = x.__len__()
+    yl = y.__len__()
+
+    index = 0
+    while (ret == 0):
+        if index >= xl and index <= yl:
+            return -1
+        if index >= yl and index <= xl:
+            return 1
+
+        ret = order.index(x[index]) - order.index(y[index])
+        index += 1
+        
+    return ret
+    
+def vocabulario():
+    words = txt_b.split()
+    ordered = sorted( words, cmp=cmp_order )
+    
+    #test_voc_a(ordered)
+    voc = ' '.join(ordered)
+
+    print '\n',voc
+
+def test_voc_a(ordered):
+    voc = voc_txt_a.split()
+    for w in voc:
+        if w != ordered[voc.index(w)]:
+            print w, ordered[voc.index(w)]
+            print 'diferentes!'
+            break
+
+def word_num_2_dec(word):
+    num = 0
+    for c in word:
+        num += order.index(c) * ( 20 ** (word.index(c)) )
+    
+    return num
+
+def is_dup(word):
+    words = txt_b.split()
+    if words.count(word) > 1:
+        True
+    else:
+        False
+        
+def num_bonitos():
+    words = txt_b.split()
+    bonitos = []
+    for word in words:
+        if not is_dup(word):
+            num = word_num_2_dec(word)
+        
+            if num > 568789 and num % 5 == 0:
+                bonitos.append(num) 
+
+    #print bonitos
+    print '\nTem', bonitos.__len__(), 'numeros bonitos'
+
+def main():
+    preposicoes()
+    verbos()
+    vocabulario()
+
+    num_bonitos()
+
+    #print '\n', word_num_2_dec('lrcvxf'), '\n22486133'
+
+
+if __name__ == '__main__':
+    main()
